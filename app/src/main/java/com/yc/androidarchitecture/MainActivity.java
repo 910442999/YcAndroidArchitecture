08e6e3880b60c9f2b21c7a36e1b0e92f7aaee7d7
910442999 <910442999@qq.com>
2018-07-25 13:24:44 +0800

    安卓框架搭建基础一

--- app/src/main/java/com/yc/androidarchitecture/MainActivity.java
+++ app/src/main/java/com/yc/androidarchitecture/MainActivity.java
@@ -1,13 +1,41 @@
 package com.yc.androidarchitecture;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.yc.homelibrary.HomeActivity;
+import com.yc.mylibrary.MyActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text1)
+    TextView mText1;
+    @BindView(R.id.text2)
+    TextView mText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.text1, R.id.text2})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.text1:
+                startActivity(new Intent(this, HomeActivity.class));
+                break;
+            case R.id.text2:
+                startActivity(new Intent(this, MyActivity.class));
+                break;
+        }
     }
 }
