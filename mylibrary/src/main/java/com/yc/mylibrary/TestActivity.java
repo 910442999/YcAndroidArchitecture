e34e42b7b28cef80a8cf7f68ee64ec8e1280fbfe
zh <2544789777@qq.com>
2018-08-16 16:41:16 +0800

    安卓框架搭建(八)路由框架ARouter实现各模块之间通信

--- mylibrary/src/main/java/com/yc/mylibrary/TestActivity.java
+++ mylibrary/src/main/java/com/yc/mylibrary/TestActivity.java
@@ -1,9 +1,11 @@
 package com.yc.mylibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+
+import java.util.Date;
 
 @Route(path = "/my/TestActivity")
 public class TestActivity extends AppCompatActivity {
@@ -12,5 +14,10 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+    }
+    @Override
+    public void finish() {
+        setResult(1002, getIntent().putExtra("arg2", new Date().toString()));
+        super.finish();
     }
 }
