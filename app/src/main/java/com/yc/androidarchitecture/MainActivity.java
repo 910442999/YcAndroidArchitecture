1655b54f092f42bdd03d5b21bf469aadb35f76b8
zh <2544789777@qq.com>
2018-08-17 12:38:52 +0800

    安卓框架搭建(九)UI适配适配方案

--- app/src/main/java/com/yc/androidarchitecture/MainActivity.java
+++ app/src/main/java/com/yc/androidarchitecture/MainActivity.java
@@ -3,6 +3,7 @@ package com.yc.androidarchitecture;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
